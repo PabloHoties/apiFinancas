305c63d4bb71d8ea884e4361eb216762b4de02a7
PabloHoties <pablohotiesmarques@gmail.com>
2024-08-06 11:57:51 -0300

    feita a organização, desenvolvimento de classes de domínio

---
+++ src/main/java/br/com/cotiinformatica/domain/categorias/services/CategoriaServiceImpl.java
@@ -0,0 +1,43 @@
+package br.com.cotiinformatica.domain.categorias.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.domain.categorias.models.dtos.CategoriaRequest;
+import br.com.cotiinformatica.domain.categorias.models.dtos.CategoriaResponse;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+	@Override
+	public CategoriaResponse criar(CategoriaRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaResponse alterar(Integer id, CategoriaRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaResponse excluir(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CategoriaResponse> consultar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaResponse obterPorId(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/br/com/cotiinformatica/domain/contas/services/ContaServiceImpl.java
@@ -0,0 +1,43 @@
+package br.com.cotiinformatica.domain.contas.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.domain.contas.models.dtos.ContaRequest;
+import br.com.cotiinformatica.domain.contas.models.dtos.ContaResponse;
+
+@Service
+public class ContaServiceImpl implements ContaService {
+
+	@Override
+	public ContaResponse criar(ContaRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ContaResponse alterar(Integer id, ContaRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ContaResponse excluir(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ContaResponse> consultar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ContaResponse obterPorId(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
