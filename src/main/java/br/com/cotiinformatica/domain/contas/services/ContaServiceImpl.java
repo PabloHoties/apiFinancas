32a080788e496f4f00c1341122bc61eae4474df8
PabloHoties <pablohotiesmarques@gmail.com>
2024-08-20 12:35:12 -0300

    feat: desenvolvimento dos métodos de "ContaServiceImpl"

--- src/main/java/br/com/cotiinformatica/domain/contas/services/ContaServiceImpl.java
+++ src/main/java/br/com/cotiinformatica/domain/contas/services/ContaServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.cotiinformatica.domain.contas.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -10,6 +11,7 @@ import br.com.cotiinformatica.domain.categorias.models.entities.Categoria;
 import br.com.cotiinformatica.domain.contas.models.dtos.ContaRequest;
 import br.com.cotiinformatica.domain.contas.models.dtos.ContaResponse;
 import br.com.cotiinformatica.domain.contas.models.entities.Conta;
+import br.com.cotiinformatica.domain.contas.models.enums.TipoConta;
 import br.com.cotiinformatica.infrastructure.repositories.CategoriaRepository;
 import br.com.cotiinformatica.infrastructure.repositories.ContaRepository;
 
@@ -18,50 +20,84 @@ public class ContaServiceImpl implements ContaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ContaRepository contaRepository;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
-	
+
 	@Override
 	public ContaResponse criar(ContaRequest request) throws Exception {
-		
+
 		Categoria categoria = categoriaRepository.findById(request.getCategoriaId())
 				.orElseThrow(() -> new IllegalArgumentException("Categoria não encontrada."));
-		
+
 		Conta conta = modelMapper.map(request, Conta.class);
 		conta.setCategoria(categoria);
-		
+
 		contaRepository.save(conta);
-		
+
 		ContaResponse response = modelMapper.map(conta, ContaResponse.class);
 		return response;
 	}
 
 	@Override
 	public ContaResponse alterar(Integer id, ContaRequest request) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		Conta conta = contaRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
+
+		Categoria categoria = categoriaRepository.findById(request.getCategoriaId())
+				.orElseThrow(() -> new IllegalArgumentException("Categoria não encontrada."));
+
+		conta.setNome(request.getNome());
+		conta.setData(request.getData());
+		conta.setPreco(request.getPreco());
+		conta.setTipo(TipoConta.valueOf(request.getTipo()));
+		conta.setCategoria(categoria);
+
+		contaRepository.save(conta);
+
+		ContaResponse response = modelMapper.map(conta, ContaResponse.class);
+		return response;
 	}
 
 	@Override
 	public ContaResponse excluir(Integer id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		Conta conta = contaRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
+
+		contaRepository.delete(conta);
+
+		ContaResponse response = modelMapper.map(conta, ContaResponse.class);
+		return response;
 	}
 
 	@Override
 	public List<ContaResponse> consultar() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Conta> contas = contaRepository.findAll();
+
+		List<ContaResponse> lista = new ArrayList<ContaResponse>();
+
+		for (Conta conta : contas) {
+			ContaResponse response = modelMapper.map(conta, ContaResponse.class);
+			lista.add(response);
+		}
+
+		return lista;
 	}
 
 	@Override
 	public ContaResponse obterPorId(Integer id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		Conta conta = contaRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
+
+		ContaResponse response = modelMapper.map(conta, ContaResponse.class);
+		return response;
 	}
 
 }
